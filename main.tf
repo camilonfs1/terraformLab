2bbec12165f5b23a42ff5e658f258f07db2ec675
camilonfs1 <camevargasrom@unal.edu.co>
2024-04-24 18:55:41 -0500

    update main

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "google" {
   project     = var.project_id
-  region      = "us-central1" # Cambia según tu región preferida
+  region      = var.region
 }
 
 resource "google_cloud_run_service" "isbn-users-ms" {
   name     = "isbn-users-ms"
-  location = "us-central1" # Cambia según tu región preferida
+  location = var.region
 
   template {
     spec {
@@ -16,20 +16,15 @@ resource "google_cloud_run_service" "isbn-users-ms" {
   }
 }
 
-resource "google_sql_database_instance" "default" {
+resource "google_sql_database_instance" "isbn-users-db" {
   name             = "isbn-users-db"
-  database_version = "POSTGRES_13"
-  region           = "us-central1" # Cambia según tu región preferida
+  database_version = "MYSQL_8_0"
+  region           = var.region
 
   settings {
     tier             = "db-f1-micro" # Puedes cambiar la configuración según tus necesidades
-    activation_policy = "ALWAYS"
-    disk_size = 10 # Cambia según tus necesidades
-    disk_autoresize = true
-    disk_type = "PD_SSD"
-    maintenance_window {
-      day  = 1
-      hour = 0
-    }
+    activation_policy = "ALWAYS" #  This specifies when the instance should be active. Can be either ALWAYS, NEVER or ON_DEMAND
+    disk_size = 10 # The size of data disk, in GB
+    disk_type = "PD_SSD" # (Optional) The type of data disk: PD_SSD or PD_HDD
   }
 }
